e7dd486cf031a2316cdd1a5ba2311cb784342591
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-22 23:16:46 +1000

    Added personality.go

---
+++ personality.go
@@ -0,0 +1,9 @@
+package main
+
+// Personality is an enum of Animal Crossing Villager personalities.
+type Personality int
+
+const (
+	// Cranky is a flavour of Animal Crossing Villager personality.
+	Cranky Personality = iota
+)
