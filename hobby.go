de08aff3d455c8984d31e8f390e3ba7a372462d5
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-23 14:55:30 +1000

    Added to hobby.go

--- hobby.go
+++ hobby.go
@@ -4,10 +4,16 @@ package main
 type Hobby int
 
 const (
+	// Education is the namespace for a education based hobby.
 	Education Hobby = iota
+	// Fashion is the namespace for a fashion based hobby.
 	Fashion
+	// Fitness is the namespace for a fitness based hobby.
 	Fitness
+	// Music is the namespace for a music based hobby.
 	Music
+	// Nature is the namespace for a nature based hobby.
 	Nature
+	// Play is the namespace for a play-like hobby.
 	Play
 )
