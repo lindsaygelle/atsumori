fcf9a673a4104c8fca8a24416c7ea4486bc32a47
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-23 13:57:38 +1000

    Added to species.go

--- species.go
+++ species.go
@@ -1,1 +1,42 @@
 package main
+
+// Species is an enum of Animal Crossing villager animal species.
+type Species int
+
+const (
+	Alligator Species = iota
+	Anteater
+	Bear
+	Bird
+	Bull
+	Cat
+	Chicken
+	Cow
+	Cub
+	Deer
+	Dog
+	Duck
+	Eagle
+	Elephant
+	Frog
+	Goat
+	Gorilla
+	Hamster
+	Hippo
+	Horse
+	Kangaroo
+	Koala
+	Lion
+	Monkey
+	Mouse
+	Octopus
+	Ostrich
+	Penguin
+	Pig
+	Rabbit
+	Rhino
+	Sheep
+	Squirrel
+	Tiger
+	Wolf
+)
