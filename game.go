f340f1ef5f437692fe695ae6103c2ec394a7f269
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-23 19:15:47 +1000

    Added game.go

--- game.go
+++ game.go
@@ -2,3 +2,17 @@ package main
 
 // Game is an enum of Animal Crossing games.
 type Game int
+
+const (
+	amiiboFestival Game = iota
+	animalCrossing
+	animalForest
+	animalForestE
+	animalForestEPlus
+	cityFolk
+	happyHomeDesigner
+	newHorizons
+	newLeaf
+	pocketCamp
+	wildWorld
+)
