dee6a1a514751b4d49c69fb83ea19234555c2607
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-23 14:47:18 +1000

    Added to villager.go

--- villager.go
+++ villager.go
@@ -1,13 +1,21 @@
 package main
 
+import "time"
+
 // Villager is a representation of an Animal Crossing villager.
 type Villager struct {
+
+	// Birthday is the birth date of the Animal Crossing villager.
+	Birthday time.Time
 
 	// Description is the verbose description of the Animal Crossing villager.
 	Description string
 
 	// Gender is the biological gender of the Animal Crossing villager.
 	Gender Gender
+
+	// Hobbies is a collection of interests for the Animal Crossing villager.
+	Hobbies []Hobby
 
 	// Name is the name of the Animal Crossing villager.
 	Name string
