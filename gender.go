692ef3ea6dfdf8127c4060897da945ca85f991fc
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-23 13:52:05 +1000

    Added to gender.go

--- gender.go
+++ gender.go
@@ -2,3 +2,10 @@ package main
 
 // Gender is a representation of an Animal Crossing villager gender.
 type Gender int
+
+const (
+	// Female is the namespace for female Animal Crossing villagers.
+	Female Gender = iota
+	// Male is the namespace for male Animal Crossing villagers.
+	Male
+)
