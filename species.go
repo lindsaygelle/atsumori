3b910494f51e8b07cb6cd0bb25a7286063eadab0
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-23 14:11:21 +1000

    Added to species.go

--- species.go
+++ species.go
@@ -4,39 +4,108 @@ package main
 type Species int
 
 const (
+	// Alligator is the namespace for all alligator-like Animal Crossing villagers.
 	Alligator Species = iota
+
+	// Anteater is the namespace for all anteater-like Animal Crossing villagers.
 	Anteater
+
+	// Bear is the namespace for all bear-like Animal Crossing villagers.
 	Bear
+
+	// Bird is the namespace for all bird-like Animal Crossing villagers.
 	Bird
+
+	// Bull is the namespace for all bull-like Animal Crossing villagers.
 	Bull
+
+	// Cat is the namespace for all cat-like Animal Crossing villagers.
 	Cat
+
+	// Chicken is the namespace for all chicken-like Animal Crossing villagers.
 	Chicken
+
+	// Cow is the namespace for all cow-like Animal Crossing villagers.
 	Cow
+
+	// Cub is the namespace for all cub-like Animal Crossing villagers.
 	Cub
+
+	// Deer is the namespace for all deer-like Animal Crossing villagers.
 	Deer
+
+	// Dog is the namespace for all dog-like Animal Crossing villagers.
 	Dog
+
+	// Duck is the namespace for all duck-like Animal Crossing villagers.
 	Duck
+
+	// Eagle is the namespace for all eagle-like Animal Crossing villagers.
 	Eagle
+
+	// Elephant is the namespace for all elephant-like Animal Crossing villagers.
 	Elephant
+
+	// Frog is the namespace for all frog-like Animal Crossing villagers.
 	Frog
+
+	// Goat is the namespace for all goat-like Animal Crossing villagers.
 	Goat
+
+	// Gorilla is the namespace for all gorilla-like Animal Crossing villagers.
 	Gorilla
+
+	// Hamster is the namespace for all hamster-like Animal Crossing villagers.
 	Hamster
+
+	// Hippo is the namespace for all hippo-like Animal Crossing villagers.
 	Hippo
+
+	// Horse is the namespace for all horse-like Animal Crossing villagers.
 	Horse
+
+	// Kangaroo is the namespace for all kangaroo-like Animal Crossing villagers.
 	Kangaroo
+
+	// Koala is the namespace for all koala-like Animal Crossing villagers.
 	Koala
+
+	// Lion is the namespace for all lion-like Animal Crossing villagers.
 	Lion
+
+	// Monkey is the namespace for all monkey-like Animal Crossing villagers.
 	Monkey
+
+	// Mouse is the namespace for all mouse-like Animal Crossing villagers.
 	Mouse
+
+	// Octopus is the namespace for all octopus-like Animal Crossing villagers.
 	Octopus
+
+	// Ostrich is the namespace for all ostrich-like Animal Crossing villagers.
 	Ostrich
+
+	// Penguin is the namespace for all penguin-like Animal Crossing villagers.
 	Penguin
+
+	// Pig is the namespace for all pig-like Animal Crossing villagers.
 	Pig
+
+	// Rabbit is the namespace for all rabbit-like Animal Crossing villagers.
 	Rabbit
+
+	// Rhino is the namespace for all rhino-like Animal Crossing villagers.
 	Rhino
+
+	// Sheep is the namespace for all sheep-like Animal Crossing villagers.
 	Sheep
+
+	// Squirrel is the namespace for all squirrel-like Animal Crossing villagers.
 	Squirrel
+
+	// Tiger is the namespace for all tiger-like Animal Crossing villagers.
 	Tiger
+
+	// Wolf is the namespace for all wolf-like Animal Crossing villagers.
 	Wolf
 )
