6d6b38612084ad6f9ca424a86da348e4044c7216
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-23 20:45:53 +1000

    Added to game.go

--- game.go
+++ game.go
@@ -4,8 +4,8 @@ package main
 type Game int
 
 const (
-	amiiboFestival Game = iota
-	animalCrossing
+	animalCrossing Game = iota
+	amiiboFestival
 	animalForest
 	animalForestE
 	animalForestEPlus
@@ -16,3 +16,20 @@ const (
 	pocketCamp
 	wildWorld
 )
+
+func (g Game) String() string {
+	return (games[g])
+}
+
+var games = [...]string{
+	"Animal-Crossing",
+	"Animal Crossing: Amiibo-Festival",
+	"Animal Crossing: Animal-Forest",
+	"Animal Crossing: Animal-Forest-E",
+	"Animal Crossing: Animal-Forest-E+",
+	"Animal Crossing: City-Folk",
+	"Animal Crossing: Happy-Home-Designer",
+	"Animal Crossing: New-Horizons",
+	"Animal Crossing: New-Leaf",
+	"Animal Crossing: Pocket-Camp",
+	"Animal Crossing: Wild-World"}
