5fd75aeb453227a70194f1a5937146f14e5c9165
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-23 13:49:25 +1000

    Added to villager.go

--- villager.go
+++ villager.go
@@ -3,9 +3,12 @@ package main
 // Villager is a representation of an Animal Crossing villager.
 type Villager struct {
 
+	// Gender is the biological gender of the Animal Crossing Villager.
+	Gender Gender
+
 	// Name is the name of the Animal Crossing villager.
 	Name string
 
 	// Personality is the personality of the Animal Crossing Villager.
-	Personality string
+	Personality Personality
 }
