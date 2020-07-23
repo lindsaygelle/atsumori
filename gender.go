7bfde6715dbd599375f30cd64ceb85d3bb8db81c
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-23 20:47:11 +1000

    Added to gender.go

--- gender.go
+++ gender.go
@@ -3,7 +3,15 @@ package main
 // Gender is an enum of villager genders found in the Animal Crossing series.
 type Gender int
 
+func (g Gender) String() string {
+	return (genders[g])
+}
+
 const (
 	female Gender = iota
 	male
 )
+
+var genders = [...]string{
+	"female",
+	"male"}
