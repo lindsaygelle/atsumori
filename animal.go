f8961feed4588106f1eb3d994e82fcb1a2b700e0
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-24 19:00:38 +1000

    Added to animal.go && game.go

--- animal.go
+++ animal.go
@@ -6,15 +6,16 @@ import "time"
 type animal struct {
 	astrology
 	color
+	game
 	gender
 	name
 	species
 
+	// ACard is the Amiibo-Card information for the Animal Crossing character.
+	ACard ACard `json:"acard"`
+
 	// Birthday is the birth date for the Animal Crossing character.
 	Birthday time.Time `json:"birthday"`
-
-	// Games is the Animal Crossing games the Animal Crossing character has appeared in.
-	Games []Game `json:"games"`
 
 	// ECard is the E-card information for the Animal Crossing character.
 	ECard ECard `json:"ecard"`
--- game.go
+++ game.go
@@ -21,15 +21,20 @@ func (g Game) String() string {
 	return (games[g])
 }
 
+// game is a composable field.
+type game struct {
+	Games []Game `json:"games"`
+}
+
 var games = [...]string{
 	"Animal-Crossing",
-	"Animal Crossing: Amiibo-Festival",
-	"Animal Crossing: Animal-Forest",
-	"Animal Crossing: Animal-Forest-E",
-	"Animal Crossing: Animal-Forest-E+",
-	"Animal Crossing: City-Folk",
-	"Animal Crossing: Happy-Home-Designer",
-	"Animal Crossing: New-Horizons",
-	"Animal Crossing: New-Leaf",
-	"Animal Crossing: Pocket-Camp",
-	"Animal Crossing: Wild-World"}
+	"Animal Crossing: Amiibo Festival",
+	"Animal Crossing: Animal Forest",
+	"Animal Crossing: Animal Forest E",
+	"Animal Crossing: Animal Forest E+",
+	"Animal Crossing: City Folk",
+	"Animal Crossing: Happy Home Designer",
+	"Animal Crossing: New Horizons",
+	"Animal Crossing: New Leaf",
+	"Animal Crossing: Pocket Camp",
+	"Animal Crossing: Wild World"}
