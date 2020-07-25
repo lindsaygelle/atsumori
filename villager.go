c19e2ecefff28d8401ca48eea24fc8dbd7c9ca42
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-26 00:26:17 +1000

    Added to villager.go

--- villager.go
+++ villager.go
@@ -5,6 +5,7 @@ type Villager struct {
 	color
 	description
 	games
+	names
 	villagerAmiiboCard
 	villagerAstrology
 	villagerBirthday
@@ -17,12 +18,9 @@ type Villager struct {
 	villagerName
 	villagerPersonality
 
-	// Aliases is a collection of alternative names of the Animal Crossing villager.
-	Aliases []ISO
-
 	// Appearance is a description of the Animal Crossing villagers appearance.
 	Appearance string
 
 	// Phrases is a collection of quotes said by the Animal Crossing villager.
-	Phrases []ISO
+	Phrases []I18N
 }
