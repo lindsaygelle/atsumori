5058d5f545143be459b7f94923512dd8ef6a4091
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-27 00:04:03 +1000

    Added to villager-coffee.go

--- villager-coffee.go
+++ villager-coffee.go
@@ -2,7 +2,7 @@ package main
 
 // VillagerCoffee is an Animal Crossing villagers coffee blend preference.
 type VillagerCoffee struct {
-	villagerCoffeeBlend
+	villagerCoffeeBeans
 	villagerCoffeeMilk
 	villagerCoffeeSugar
 }
