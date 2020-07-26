f6336b20dd83ce6fa2721633f8906f2c9fef4b5a
Linneeeeeeee <lindsay.gelle@gmail.com>
2020-07-26 20:14:29 +1000

    Update villager-coffee-sugar.go

--- villager-coffee-sugar.go
+++ villager-coffee-sugar.go
@@ -1,9 +1,13 @@
 package main
 
+// VillagerCoffeeSugar is an enum of spoonfuls of sugar an Animal Crossing villager prefers in their coffee.
 type VillagerCoffeeSugar uint
 
+// villagerCoffeeSugar is a composable struct.
 type villagerCoffeeSugar struct {
-	Sugar VillagerCoffeeSugar
+	
+	// Sugar is number of spoonfuls a villager prefers in their coffee.
+	Sugar VillagerCoffeeSugar `json:"sugar"`
 }
 
 const (
