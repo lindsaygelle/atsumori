e43d76d4fa28d8c03d5764112cbb0899757c19cb
Linneeeeeeee <lindsay.gelle@gmail.com>
2020-07-26 20:10:30 +1000

    Update villager-coffee-blend.go

--- villager-coffee-blend.go
+++ villager-coffee-blend.go
@@ -1,12 +1,12 @@
 package main
 
-// VillagerCoffeeBlend is an enum of coffee blends an Animal Crossing villager prefers.
+// VillagerCoffeeBlend is an enum of coffee blends an Animal Crossing villager enjoys.
 type VillagerCoffeeBlend uint
 
 // villagerCoffeeBlend is a composable struct.
 type villagerCoffeeBlend struct {
 
-	// Blend is the coffee blend of coffee the Animal Crossing villager prefers.
+	// Blend is the coffee blend of coffee the Animal Crossing villager enjoys.
 	Blend VillagerCoffeeBlend `json:"blend"`
 }
 
