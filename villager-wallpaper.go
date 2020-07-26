0796179b351a574dcf219aa825ef1bbe57a792f9
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-26 15:28:27 +1000

    Added to villager-wallpaper.go

--- villager-wallpaper.go
+++ villager-wallpaper.go
@@ -1,7 +1,11 @@
 package main
 
+// VillagerWallpaper is an enum of wallpaper decorating an Animal Crossing villagers home.
 type VillagerWallpaper uint
 
+// villagerWallpaper is a composable struct.
 type villagerWallpaper struct {
+
+	// Wallpaper is a wallpaper used for home decoration.
 	Wallpaper VillagerWallpaper `json:"wallpaper"`
 }
