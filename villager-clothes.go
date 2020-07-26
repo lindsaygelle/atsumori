9fdd5f812f5fcce6cdf479d659b3b68fd966828e
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-26 15:43:10 +1000

    Added villager-clothes.go

---
+++ villager-clothes.go
@@ -0,0 +1,19 @@
+package main
+
+// VillagerClothes is an enum of clothes worn by an Animal Crossing villager.
+type VillagerClothes uint
+
+func (v VillagerClothes) String() string {
+	return (clothes[v])
+}
+
+// villagerClothes is a composable struct.
+type villagerClothes struct {
+
+	// Clothes are the clothes worn by the Animal Crossing villager.
+	Clothes VillagerClothes `json:"clothes"`
+}
+
+const ()
+
+var clothes = [...]string{}
