c006183d32f2099d521ad422265079edfbf97ed2
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-27 00:17:59 +1000

    Added to villager-astrology.go

--- villager-astrology.go
+++ villager-astrology.go
@@ -14,17 +14,38 @@ func (v villagerAstrology) SetAstrology(x VillagerAstrology) {
 	v.Astrology = x
 }
 
-const (
-	villagerAstrologyAquarius    VillagerAstrology = "Aquarius"
-	villagerAstrologyAries       VillagerAstrology = "Aries"
-	villagerAstrologyCancer      VillagerAstrology = "Cancer"
-	villagerAstrologyCapricorn   VillagerAstrology = "Capricorn"
-	villagerAstrologyGemini      VillagerAstrology = "Gemini"
-	villagerAstrologyLeo         VillagerAstrology = "Leo"
-	villagerAstrologyLibra       VillagerAstrology = "Libra"
-	villagerAstrologyPisces      VillagerAstrology = "Pisces"
-	villagerAstrologySagittarius VillagerAstrology = "Sagittarius"
-	villagerAstrologyScorpio     VillagerAstrology = "Scorpio"
-	villagerAstrologyTaurus      VillagerAstrology = "Taurus"
-	villagerAstrologyVirgo       VillagerAstrology = "Virgo"
-)
+// villagerAstrologyAquarius is the astrological sign for Aquarius
+const villagerAstrologyAquarius VillagerAstrology = "Aquarius"
+
+// villagerAstrologyAries is the astrological sign for Aries.
+const villagerAstrologyAries VillagerAstrology = "Aries"
+
+// villagerAstrologyCancer is the astrological sign for Cancer
+const villagerAstrologyCancer VillagerAstrology = "Cancer"
+
+// villagerAstrologyCapricorn is the astrological sign for Capricorn.
+const villagerAstrologyCapricorn VillagerAstrology = "Capricorn"
+
+// villagerAstrologyGemini is the astrological sign for Gemini.
+const villagerAstrologyGemini VillagerAstrology = "Gemini"
+
+// villagerAstrologyLeo is the astrological sign for Leo.
+const villagerAstrologyLeo VillagerAstrology = "Leo"
+
+// villagerAstrologyLibra is the astrological sign for Libra.
+const villagerAstrologyLibra VillagerAstrology = "Libra"
+
+// villagerAstrologyPisces is the astrological sign for Pisces.
+const villagerAstrologyPisces VillagerAstrology = "Pisces"
+
+// villagerAstrologySagittarius is the astrological sign for Sagittarius.
+const villagerAstrologySagittarius VillagerAstrology = "Sagittarius"
+
+// villagerAstrologyScorpio is the astrological sign for Scorpio.
+const villagerAstrologyScorpio VillagerAstrology = "Scorpio"
+
+// villagerAstrologyTaurus is the astrological sign for Taurus.
+const villagerAstrologyTaurus VillagerAstrology = "Taurus"
+
+// villagerAstrologyVirgo is the astrological sign for Virgo.
+const villagerAstrologyVirgo VillagerAstrology = "Virgo"
