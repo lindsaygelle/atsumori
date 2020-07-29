0023e826ff6d49a3bfed5f19196b6011eb4acd3b
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-29 20:16:15 +1000

    Added villager_fear.go

---
+++ villager_fear.go
@@ -0,0 +1,15 @@
+package main
+
+type villagerFear uint8
+
+type villagersFear struct {
+	Fear *villagerFear
+}
+
+const (
+	villagerFearBugMask villagerFear = iota
+	villagerFearMonsterMask
+	villagerFearMummyMask
+	villagerFearSkeletonHood
+	villagerFearWerewolfHood
+)
