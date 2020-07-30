abf54984157f0728ffa2e36af3df63fa310ba25a
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-30 19:10:19 +1000

    Added to villager_skill.go

--- villager_skill.go
+++ villager_skill.go
@@ -5,3 +5,21 @@ type villagerSkill uint16
 type villagersSkill struct {
 	Skill *villagerSkill
 }
+
+const (
+	villagersSkillBellyDancing villagerSkill = iota
+	villagersSkillCalligraphy
+	villagersSkillCrammingForTests
+	villagersSkillEatingQuickly
+	villagersSkillFishing
+	villagersSkillForgettingStuff
+	villagersSkillKaraoke
+	villagersSkillLimboing
+	villagersSkillMakingCrafts
+	villagersSkillOversleeping
+	villagersSkillSkimmingBooks
+	villagersSkillTiptoeing
+	villagersSkillTrivia
+	villagersSkillWakingUpEarly
+	villagersSkillWritingBackwards
+)
