986a4b6603a9ccd4c704d79a44b2f0cc3a6ee02c
lindsaygelle <lindsay.gelle@gmail.com>
2020-08-02 18:57:30 +1000

    Added to villager_skill.*

--- villager_skill.go
+++ villager_skill.go
@@ -54,6 +54,7 @@ const (
 	villagerSkillJuggling
 	villagerSkillJumpingRope
 	villagerSkillKaraoke
+	villagerSkillLiftingWeights
 	villagerSkillLimboing
 	villagerSkillMagicTricks
 	villagerSkillMakingBudgets
