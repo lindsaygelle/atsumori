461047656f1408e8bc39c44881d3faf32c7ff14b
lindsaygelle <lindsay.gelle@gmail.com>
2020-08-09 12:14:47 +1000

    Added to villager_birth_month*.go

--- villager_birth_month.go
+++ villager_birth_month.go
@@ -2,6 +2,14 @@ package atsumori
 
 import "time"
 
+var _ villagerBirthMonth = villagersBirthMonth{}
+
+type villagerBirthMonth interface {
+	BirthMonth() uint8
+}
+
 type villagersBirthMonth struct {
 	VillagerBirthMonth time.Month
 }
+
+func (v villagersBirthMonth) BirthMonth() uint8 { return uint8(v.VillagerBirthMonth) }
