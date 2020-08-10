5d26ecd2bbbf5237b69841254f3bc917efdb3882
lindsaygelle <lindsay.gelle@gmail.com>
2020-08-10 23:19:12 +1000

    Added a_alice.go

---
+++ a_alice.go
@@ -0,0 +1,45 @@
+package atsumori
+
+// Alice is an Animal Crossing villager
+var Alice = villager{
+	aliceAstrology,
+	aliceBirthDay,
+	aliceBirthMonth,
+	aliceBubbleColor,
+	aliceCategory,
+	aliceClothes,
+	aliceClothesColors,
+	aliceFlooring,
+	aliceFurniture,
+	aliceGames,
+	aliceGender,
+	aliceInterest,
+	aliceName,
+	aliceNameColor,
+	aliceMusic,
+	alicePersonality,
+	aliceSpecies,
+	aliceStyle,
+	aliceWallpaper}
+
+var (
+	aliceAstrology     = villagersAstrology{}
+	aliceBirthDay      = villagersBirthDay{}
+	aliceBirthMonth    = villagersBirthMonth{}
+	aliceBubbleColor   = villagersBubbleColor{villagerBubbleColorE8B010}
+	aliceCategory      = villagersCategory{villagerCategoryB}
+	aliceClothes       = villagersClothes{} // 4729
+	aliceClothesColors = villagersClothesColors{[2]VillagerClothesColor{villagerClothesColorRed, villagerClothesColorPink}}
+	aliceFlooring      = villagersFlooring{villagerFlooringLightParquetFlooring}
+	aliceFurniture     = villagersFurniture{[]VillagerFurniture{villagerFurnitureRattanBed, villagerFurnitureRattanLowTable, villagerFurnitureMonstera, villagerFurnitureRattanArmchair, villagerFurnitureRattanWardrobe, villagerFurnitureOpenFrameKitchen, villagerFurnitureRattanVanity, villagerFurnitureBotanicalRug, villagerFurnitureCoconutWallPlanter, villagerFurnitureWallMountedTV50In, villagerFurnitureRattanEndTable, villagerFurniturePortableRecordPlayer}}
+	aliceGames         = villagersGames{[]VillagerGame{}} // TBD
+	aliceGender        = villagersGender{[2]VillagerGender{villagerGenderFemale}}
+	aliceInterest      = villagersInterest{villagerInterestEducation}
+	aliceName          = villagersName{villagerNameAlice}
+	aliceNameColor     = villagersNameColor{villagerNameColorFFFCE9}
+	aliceMusic         = villagersMusic{} // Aloha K.K.
+	alicePersonality   = villagersPersonality{villagerPersonalityNormal}
+	aliceSpecies       = villagersSpecies{[2]VillagerSpecies{villagerSpeciesKoala}}
+	aliceStyle         = villagersStyle{[2]VillagerStyle{villagerStyleCute, villagerStyleCute}}
+	aliceWallpaper     = villagersWallpaper{villagerWallpaperWhiteBotanicalTileWall}
+)
