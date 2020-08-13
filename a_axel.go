18e58da0e18cfa65b03f1858398611a21946756e
lindsaygelle <lindsay.gelle@gmail.com>
2020-08-13 23:48:16 +1000

    Added a_axel.go

---
+++ a_axel.go
@@ -0,0 +1,47 @@
+package atsumori
+
+import "time"
+
+// Axel is an Animal Crossing villager
+var Axel = villager{
+	axelAstrology,
+	axelBirthDay,
+	axelBirthMonth,
+	axelBubbleColor,
+	axelCategory,
+	axelClothes,
+	axelClothesColors,
+	axelFlooring,
+	axelFurniture,
+	axelGames,
+	axelGender,
+	axelInterest,
+	axelName,
+	axelNameColor,
+	axelMusic,
+	axelPersonality,
+	axelSpecies,
+	axelStyle,
+	axelWallpaper}
+
+var (
+	axelAstrology     = villagersAstrology{villagerAstrologyAries}
+	axelBirthDay      = villagersBirthDay{23}
+	axelBirthMonth    = villagersBirthMonth{time.March} // 3
+	axelBubbleColor   = villagersBubbleColor{villagerBubbleColor459ABA}
+	axelCategory      = villagersCategory{villagerCategoryB}
+	axelClothes       = villagersClothes{villagerClothesKanjiTee} // 3575
+	axelClothesColors = villagersClothesColors{[2]VillagerClothesColor{villagerClothesColorGreen, villagerClothesColorWhite}}
+	axelFlooring      = villagersFlooring{villagerFlooringBlueRubberFlooring}
+	axelFurniture     = villagersFurniture{[]VillagerFurniture{villagerFurnitureWoodenBlockStereo, villagerFurnitureTennisTable, villagerFurnitureThrowbackWallClock, villagerFurnitureThrowbackRaceCarBed, villagerFurnitureCandyMachine, villagerFurnitureWoodenBlockBookshelf, villagerFurnitureWoodenBlockstool, villagerFurnitureThrowbackRocket, villagerFurnitureDigitalAlarmClock, villagerFurnitureWoodenBlockBench, villagerFurnitureThrowbackContainer, villagerFurnitureToyBox}}
+	axelGames         = villagersGames{[]VillagerGame{}} // TBD
+	axelGender        = villagersGender{[2]VillagerGender{villagerGenderMale}}
+	axelInterest      = villagersInterest{villagerInterestFitness}
+	axelName          = villagersName{villagerNameAxel}
+	axelNameColor     = villagersNameColor{villagerNameColorFFFCE9}
+	axelMusic         = villagersMusic{} // K.K. Dixie
+	axelPersonality   = villagersPersonality{villagerPersonalityJock}
+	axelSpecies       = villagersSpecies{[2]VillagerSpecies{villagerSpeciesElephant}}
+	axelStyle         = villagersStyle{[2]VillagerStyle{villagerStyleActive, villagerStyleSimple}}
+	axelWallpaper     = villagersWallpaper{villagerWallpaperBluePlayroomWall}
+)
--- villager.go
+++ villager.go
@@ -74,5 +74,6 @@ var (
 		Audie,
 		Aurora,
 		Ava,
-		Avery}
+		Avery,
+		Axel}
 )
