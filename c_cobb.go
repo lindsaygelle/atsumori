06b802799407a7cc89380091b290c7220c76a71b
lindsaygelle <lindsay.gelle@gmail.com>
2020-08-21 00:45:07 +1000

    Added c_cobb.go

---
+++ c_cobb.go
@@ -0,0 +1,47 @@
+package atsumori
+
+import "time"
+
+// Cobb is an Animal Crossing villager
+var Cobb = villager{
+	cobbAstrology,
+	cobbBirthDay,
+	cobbBirthMonth,
+	cobbBubbleColor,
+	cobbCategory,
+	cobbClothes,
+	cobbClothesColors,
+	cobbFlooring,
+	cobbFurniture,
+	cobbGames,
+	cobbGender,
+	cobbInterest,
+	cobbName,
+	cobbNameColor,
+	cobbMusic,
+	cobbPersonality,
+	cobbSpecies,
+	cobbStyle,
+	cobbWallpaper}
+
+var (
+	cobbAstrology     = villagersAstrology{villagerAstrologyLibra}
+	cobbBirthDay      = villagersBirthDay{7}
+	cobbBirthMonth    = villagersBirthMonth{time.October} // 10
+	cobbBubbleColor   = villagersBubbleColor{villagerBubbleColorA8E989}
+	cobbCategory      = villagersCategory{villagerCategoryB}
+	cobbClothes       = villagersClothes{villagerClothesFlannelShirt} // 8946
+	cobbClothesColors = villagersClothesColors{[2]VillagerClothesColor{villagerClothesColorWhite, villagerClothesColorBlue}}
+	cobbFlooring      = villagersFlooring{villagerFlooringConcreteFlooring}
+	cobbFurniture     = villagersFurniture{[]VillagerFurniture{villagerFurnitureSleepingBag, villagerFurnitureWoodenBookshelf, villagerFurnitureWoodenBookshelf, villagerFurnitureWoodenBookshelf, villagerFurnitureDocumentStack, villagerFurnitureHomeworkSet, villagerFurnitureIronwoodDIYWorkbench, villagerFurnitureSloppyRug, villagerFurnitureStackOfBooks, villagerFurnitureWhiteboard, villagerFurnitureCardboardBox, villagerFurniturePortableRadio, villagerFurnitureScatteredPapers, villagerFurnitureCardboardSofa, villagerFurnitureScatteredPapers, villagerFurnitureStackOfBooks, villagerFurnitureStackOfBooks}}
+	cobbGames         = villagersGames{[]VillagerGame{}} // TBD
+	cobbGender        = villagersGender{[2]VillagerGender{villagerGenderMale}}
+	cobbInterest      = villagersInterest{villagerInterestEducation}
+	cobbName          = villagersName{villagerNameCobb}
+	cobbNameColor     = villagersNameColor{villagerNameColor878E86}
+	cobbMusic         = villagersMusic{} // Pondering
+	cobbPersonality   = villagersPersonality{villagerPersonalityJock}
+	cobbSpecies       = villagersSpecies{[2]VillagerSpecies{villagerSpeciesPig}}
+	cobbStyle         = villagersStyle{[2]VillagerStyle{villagerStyleSimple, villagerStyleActive}}
+	cobbWallpaper     = villagersWallpaper{villagerWallpaperGrayShantyWall}
+)
--- villager.go
+++ villager.go
@@ -146,5 +146,6 @@ var (
 		Clay,
 		Cleo,
 		Clyde,
-		Coach}
+		Coach,
+		Cobb}
 )
